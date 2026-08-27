e5e2b62c6253a5bf8588a8802cd8eb438a43b1f9
allhailbotgod <codenameopvirus@gmail.com>
2026-08-27 23:26:25 +0100

    Checkpoint: Aug 27 11:26 pm

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from app.routers import api_router
 app = FastAPI(
     title="Rapid Incident Response API",
     summary="REST API for real-time emergency reporting, incident management, dispatch coordination, and response tracking.",
-    description="This Rapid Incident Response API provides secure endpoints for reporting emergencies, managing incidents, dispatching response agencies, nd tracking response progress in real time. It supports public user, emergency dispatchers, hospitals, police, and fire service personnel through role-based access control, enabling faster coordination and efficient emergency response.",
+    description="This Rapid Incident Response API provides secure endpoints for reporting emergencies, managing incidents, dispatching response agencies, and tracking response progress in real time. It supports public user, emergency dispatchers, hospitals, police, and fire service personnel through role-based access control, enabling faster coordination and efficient emergency response.",
     version="1.0.0",
 )
 
